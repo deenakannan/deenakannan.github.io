7a6600abb2a0937fab024a6a49f047d936551683
deenakannan <deenakannan100@gmail.com>
2020-05-09 00:38:38 +0530

    webs

---
+++ js/showimage.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$("div").each(function(i){
+    $(img).append("<img src='images/img_"+i+".jpg' />");
+   
+});
+$("div").each(function(i){
+    $(hre).append("<a href='images/img_"+i+".jpg' />");
+
+});
+});
